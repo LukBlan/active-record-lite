1418964e4ec9f3b426c5511667d571fe26f0eb92
lukblan <luk.blanko@gmail.com>
2024-06-29 10:38:22 -0300

    Update database seed

--- scripts/db_seed.sql
+++ scripts/db_seed.sql
@@ -1,8 +1,23 @@
 CREATE TABLE people (
-    name varchar(255)
+    name varchar(255),
+    surname varchar(255),
+    age int
+);
+
+CREATE TABLE animals (
+  name VARCHAR(255),
+  race VARCHAR(255),
+  color VARCHAR(255)
 );
 
 INSERT INTO
-    people (name)
+    people (name, surname, age)
 VALUES
-    ('Lucas')
+    ('Lucas', 'blanco', 28),
+    ('Fede', 'blanco', 30);
+
+INSERT INTO
+    animals (name, race, color)
+VALUES
+    ('Tomas', 'dog1', 'blue'),
+    ('Max', 'dog1', 'red')
