03e0b3f96dcc223d9e3c47cc23da646f811b03cc
lukblan <luk.blanko@gmail.com>
2024-06-29 09:59:36 -0300

    Add model setters and getter

--- lib/sql_object.rb
+++ lib/sql_object.rb
@@ -10,8 +10,6 @@ class SqlObject
         #{self.table_name}
     SQL
     ).columns
-
-
   end
 
   def self.table_name
@@ -21,9 +19,39 @@ class SqlObject
   def self.table_name=(table_name)
      @table_name = table_name
   end
+
+  def initialize(hash)
+    hash.each do |key, value|
+      string_key = key.to_s
+      raise ArgumentError.new("Invalid argument #{key} don't exist") if !self.class.columns.include?(string_key)
+      send("#{string_key}=".to_sym, value)
+    end
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def self.finalize!
+    columns = self.columns
+
+    columns.each do |column_name|
+      define_method(column_name) do
+        self.attributes
+        @attributes[column_name]
+      end
+
+      define_method("#{column_name}=") do |value|
+        self.attributes
+        @attributes[column_name] = value
+      end
+    end
+  end
 end
 
 class Person < SqlObject
+  self.finalize!
 end
 
-# p Person.columns
+person = Person.new({name: "Lucas"})
+p person.attributes
